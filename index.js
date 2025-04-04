a3ac0436f8fe3bf8c75b5d1ef5b686ddff01f3f7
awatersny <awatersny@gmail.com>
2025-04-04 15:42:31 -0400

    Define template engine initially

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(express.static("./styles"))
 app.engine("view", (filePath, options, callback) => {
   fs.readFile(filePath, (err, content) => {
     if(err) return callback(err)
+
+    const rendered = content.toString()
+      .replaceAll("#title#", options.title)
+      .replace("#content#", options.content)
+    return callback(null, rendered)
   })
 })
 
