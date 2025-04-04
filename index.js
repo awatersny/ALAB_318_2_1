ba21e01e869e5b1bb6319d47728244121a1dcbbd
awatersny <awatersny@gmail.com>
2025-04-04 15:26:57 -0400

    Declare the template engine

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const port = 3000
 
 app.use(express.static("./styles"))
 
+app.engine("view", (filePath, options, callback) => {
+  fs.readFile(filePath, (err, content) => {
+    if(err) return callback(err)
+  })
+})
+
