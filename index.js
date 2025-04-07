0668196868fe5392da5677faf4120ced5620c165
awatersny <awatersny@gmail.com>
2025-04-07 10:24:59 -0400

    Kinda emulate ejs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const indexRoutes = require("./routes/index");
 const express = require("express")
 const fs = require("fs")
 const app = express()
@@ -5,19 +6,24 @@ const port = 3000
 
 app.use(express.static("./styles"))
 
-app.engine("view", (filePath, options, callback) => {
+// app.get("/", (req, res) => {
+  //   res.send("Keeping it simple.");
+  // });
+  
+app.engine("mobes", (filePath, options, callback) => {
   fs.readFile(filePath, (err, content) => {
     if(err) return callback(err)
-
+      
     const rendered = content.toString()
-      .replaceAll("#title#", options.title)
-      .replace("#content#", options.content)
+    .replaceAll("<%= title %>", options.title)
+    .replace("<%= content %>", options.content)
     return callback(null, rendered)
   })
 })
-
-app.set("views", "/views")
-app.set("view engine", "view")
+  
+app.set("views", "./views")
+app.set("view engine", "mobes")
+app.use("/", indexRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`)
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+
+router.use((req, res, next) => {
+  console.log("User Request Time: ", Date.now())
+  next()
+})
+
+router.route("/").get((req, res) => {
+  const options = {
+    title: "Rendering Views with Express",
+    content:
+      "Here, we've created a basic template engine using <code>app.engine()</code> \
+      and the <code>fs</code> module, then used <code>res.render</code> to \
+      render this page using custom content within the template.<br><br> \
+      Generally, you won't want to create your own view engines, \
+      but it important to understand how they work behind the scenes. \
+      For a look at some popular view engines, check out the documentation for \
+      <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
+      <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
+      <a href='https://www.npmjs.com/package/ejs'>EJS</a>. \
+      More complete front-end libraries like React, Angular, and Vue \
+      also have Express integrations.",
+  };
+
+  res.render("index", options);
+});
+
+module.exports = router;
